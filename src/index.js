5b5832aee44e10bb91ea0362bf4695aedaca64fd
8ceg6 <8ceg6@comcast.net>
2020-10-06 10:17:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    const form = document.querySelector("form#create-task-form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const inputTag = document.querySelector("input#new-task-description");
+        const taskInput = inputTag.value;
+
+
+        const newTask = document.createElement("li");
+        newTask.innerText = taskInput;
+
+        const ulTask = document.querySelector("ul#tasks");
+        ulTask.append(newTask);
+        inputTag.value = "";
+
+    })
+
 });
